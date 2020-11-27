63d0a4ce3b2a2fb0d5bbcc622c084b8dd1931c4a
ekzyis <r.gill@stud.uni-heidelberg.de>
2020-11-27 12:15:07 +0100

    Plot data

--- thesis/figures/pyplot/plot.py
+++ thesis/figures/pyplot/plot.py
@@ -4,6 +4,9 @@ import csv
 from dataclasses import dataclass
 from typing import List
 from pprint import pprint
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
 
 @dataclass
 class DataPoint:
@@ -22,15 +25,22 @@ class PlotData:
   name: str
   data: List[DataPoint]
 
+  def x(self):
+    return [d.action for d in self.data]
+
+  def y(self):
+    return [d.average() for d in self.data]
+
 def read_data(file: str) -> PlotData:
   with open(file) as csvfile:
     r = csv.reader(csvfile, delimiter='|')
     data = []
+    next(r)
     for row in r:
       if len(row) == 3:
-        data.append(CacheDataPoint(action=row[0], cache=row[1] == 'x', samples=row[2].split(',')))
+        data.append(CacheDataPoint(action=int(row[0]), cache=row[1] == 'x', samples=[int(x) for x in row[2].split(',')]))
       else:
-        data.append(DataPoint(action=row[0], samples=row[1].split(',')))
+        data.append(DataPoint(action=int(row[0]), samples=[int(x) for x in row[1].split(',')]))
     return PlotData(name=file.replace('benchmark/', '').replace('.benchmark.csv', ''), data=data)
 
 data: List[PlotData] = []
@@ -40,3 +50,12 @@ data.append(read_data('benchmark/navsystem-central.benchmark.csv'))
 data.append(read_data('benchmark/navsystem-linear.benchmark.csv'))
 
 pprint(data)
+
+for d in data:
+  x = d.x()
+  y = d.y()
+  fig, ax = plt.subplots()
+  ax.plot(x, y, '-o')
+  ax.set(title=d.name, xlabel='action', ylabel='Time [ms]')
+  plt.show()
+
